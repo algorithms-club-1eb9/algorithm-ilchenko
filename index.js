08ebc9f0afa45e74fe8b59067884cc3db083428f
vadimilchenko <v.ilchenko@8ways.ch>
2018-03-17 23:47:26 +0200

    calculator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const binarySearch = require('./lib/binary-search');
 const LinkedList = require('./lib/linked-list');
 const BracketsValidator = require('./lib/brackets-validator');
+const Calculator = require('./lib/calculator');
 const DynamicConnectivity = require('./lib/dynamic-connectivity');
 const Sort = require('./lib/sort');
 
@@ -8,6 +9,7 @@ module.exports = {
 	binarySearch,
 	LinkedList,
 	BracketsValidator,
+	Calculator,
 	DynamicConnectivity,
 	Sort
 };
