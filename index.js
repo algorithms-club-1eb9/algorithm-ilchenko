72014e259938bab4c924aaf4e791d0849d8ae25b
vadimilchenko <v.ilchenko@8ways.ch>
2018-03-17 12:36:55 +0200

    start sortin algorythms

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const binarySearch = require('./lib/binary-search');
 const LinkedList = require('./lib/linked-list');
 const DynamicConnectivity = require('./lib/dynamic-connectivity');
+const Sort = require('./lib/sort');
 
 module.exports = {
 	binarySearch,
 	LinkedList,
-	DynamicConnectivity
+	DynamicConnectivity,
+	Sort,
 };
